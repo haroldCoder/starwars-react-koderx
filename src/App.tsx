620306899dcca3b32d901759b938174c1d42b645
haroldCoder <haroldc2005@gmail.com>
2024-02-10 21:14:29 -0500

    implement Home component in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   const [mylaminas, setMylaminas] = useState<Array<{
@@ -16,7 +17,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login setIsLogin={setIsLogin} />} />
+          <Route path='/' element={!isLogin ? <Login setIsLogin={setIsLogin} /> : <Home laminas={mylaminas} />} /* si el usuario aun,
+           no esta loggeado seguira apareciendo el Componente Login, en caso contrario, aparecera el home, con el album */ />
         </Routes>
       </BrowserRouter>
     </>
