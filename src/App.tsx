188b091e8605f8635acb87f050204416ee915a10
haroldCoder <haroldc2005@gmail.com>
2024-02-10 22:06:32 -0500

    create chronometer in app to drive open a new sobre

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from './components/Login';
@@ -14,6 +14,25 @@ function App() {
 
   const [isLogin, setIsLogin] = useState<boolean>(false);
 
+  const [time, setTime] = useState<{
+    minute: number,
+    seconds: number
+  }>({
+    minute: 0,
+    seconds: 59
+  }) // crear stado para manejar el tiempo de espera para abrir otro sobre.
+
+  useEffect(() => {
+    if (time.seconds == 0) {
+      setTime((prev) => {
+        return ({
+          minute: 0,
+          seconds: 59
+        })
+      })
+    }
+  }, [time]) // este useEffect estara atento del el stado del tiempo, en cuanto los segundo lleguen a 0 se vuelve a resetear de fabrica
+
   return (
     <>
       <BrowserRouter>
@@ -23,6 +42,13 @@ function App() {
            no esta loggeado seguira apareciendo el Componente Login, en caso contrario, aparecera el home, con el album */ />
         </Routes>
       </BrowserRouter>
+      {
+        isLogin ? /* si el usuario ya esta logueado, abra una especie de ventana emergente en la parte posterior de abajo, que nos muestra el tiempo para abrir otro sobre */
+          <div className='absolute top-[90%] left-[90%]'>
+            <h2 className={` ${time.seconds < 59 ? 'text-red-700' : 'text-green-600' /* este sera un indicador, para que el usuario sepa cuando hay disponibilidad */} text-xl font-semibold`}>{time.minute}:{time.seconds}</h2>
+          </div>
+          : null
+      }
     </>
   )
 }
