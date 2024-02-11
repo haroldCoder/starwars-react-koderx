1b074e6ea7649ee9271016e06e46c636b0c29f4f
haroldCoder <haroldc2005@gmail.com>
2024-02-10 21:10:26 -0500

    create component Home and album

---
+++ src/components/Album.tsx
@@ -0,0 +1,55 @@
+import error from "../assets/404.png"
+
+export default function Album({laminas}: {laminas: {
+  movies: Films[];
+  characters: People[];
+  ships: Starship[]; /* implementar los tipos predefinidos en el archivo .d.ts */
+}[]}) {
+
+
+  return (
+    <div className={`flex ${laminas.length == 0 ? 'justify-center items-center p-20' : 'p-10'}`}>
+      {
+        laminas.length == 0 ? /* si no hay laminas almacenadas, mostrar imagen de 404 */
+          <img src={error} alt="404" />
+        : /* de lo contrario mostrar todos los items, con los respectivos datos */
+        <section className="gap-y-10 flex flex-col">
+          <div>
+            <h2 className="text-3xl text-gray-600 font-semibold">Peliculas</h2>
+            {
+              laminas.map((e)=>(
+                e.movies.length != 0 ? 
+                  e.movies.map((mv)=>( /* hacer doble mapeo, para acceder a un item especifico */
+                    <h1 className="text-green-500">{mv.title}</h1>
+                  ))
+                : null /* si no hay elementos en esa propiedad, no retornar nada */
+              ))
+            }
+          </div>
+          <div>
+            <h2 className="text-3xl text-gray-600 font-semibold">Personajes</h2>
+            {
+              laminas.map((e)=>(
+                e.characters.map((mv)=>(
+                  <h1 className="text-green-500">{mv.name}</h1>
+                ))
+              ))
+            }
+          </div>
+          <div>
+            <h2 className="text-3xl text-gray-600 font-semibold">Naves spaciales</h2>
+            {
+              laminas.map((e)=>(
+                e.ships.length != 0 ?
+                e.ships.map((mv)=>(
+                  <h1 className="text-green-500">{mv.name}</h1>
+                ))
+                : null
+              ))
+            }
+          </div>
+        </section>
+      }
+    </div>
+  )
+}
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import Album from './Album'
+
+export default function Home({laminas}: {laminas: {
+  movies: Films[];
+  characters: People[];
+  ships: Starship[];
+}[]}) /* recibir el stado laminas como prop, para acceder a toda la informacion, de las laminas guardadas */ {
+  return (
+    <div>
+        <Album laminas={laminas} /* usar album para mostrar todas las laminas, y pasarle esta como prop */ /> 
+    </div>
+  )
+}
