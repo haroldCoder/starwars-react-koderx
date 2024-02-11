957154b93b0677cc84f02c6ae75d8faf932b76c4
haroldCoder <haroldc2005@gmail.com>
2024-02-11 14:39:29 -0500

    add all attributes of types

--- src/swapi.d.ts
+++ src/swapi.d.ts
@@ -5,21 +5,32 @@ type People = {
     hair_color: string,
     skin_color: string,
     eye_color: string,
+    birth_year: string,
     gender: string,
     homeworld: string,
     films: string[],
     starships: string[],
+    species: string[],
+    vehicles: string[],
+    created: string,
+    edited: string,
     url: string
 }
 
 type Films = {
     title: string,
-    episode: number,
+    episode_id: number,
+    opening_crawl: string,
     director: string,
     producer: string,
+    release_date: string,
     characters: string[],
-    planents: string[],
+    planets: string[],
     starships: string[],
+    vehicles: string[],
+    species: string[],
+    created: string,
+    edited: string,
     url: string
 }
 
@@ -27,9 +38,19 @@ type Starship = {
     name: string,
     model: string,
     manufacturer: string,
+    cost_in_credits: string,
     length: string,
+    max_atmosphering_speed: string,
+    crew: string,
     passengers: string,
+    cargo_capacity: string,
     consumables: string,
+    hyperdrive_rating: string,
+    MGLT: string,
+    starship_class: string,
+    pilots: string[],
     films: string[],
+    created: string,
+    edited: string,
     url: string
 }
